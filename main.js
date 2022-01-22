1cb136d4cbc1a1c1f2000c49529a09a6e4f39408
XavierOw <82783594+XavierOw@users.noreply.github.com>
2022-01-22 18:23:02 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ leftWristX=0;
 rightWristY = 0;
 leftWristY=0;
 rightWristX=0;
-
+scoreleftwrist= 0;
 function preload()
 {
     song = loadSound("music.mp3");
@@ -30,6 +30,19 @@ function setup()
 function draw()
 {
     image(video, 0, 0, 600, 500);
+
+    fill("red")
+    stroke("red")
+
+    if(scoreleftwrist>0.2){
+
+    circle(leftWristX, leftWristY, 20);
+    InNumberLeftWristY = Number(leftWristY);
+    remove_decimals= floor(InNumberLeftWristY)
+    volume = remove_decimals/500;
+    document.getElementById("volume").innerHTML = "volume = " + volume
+    song.setVolume(volume);
+    } 
 }
 
 function play()
@@ -44,7 +57,11 @@ function gotPoses(results)
 {
     if(results.length>0)
     {
+
         console.log(results);
+        scoreleftwrist = results[0].pose.keypoints[9].score;
+        console.log("scoreleftwrist = " + scoreleftwrist)
+
         leftWristX= results[0].pose.leftWrist.x;
         leftWristY= results[0].pose.leftWrist.y;
         console.log("leftWristX =" + leftWristX +"leftWristY"+ leftWristY)
